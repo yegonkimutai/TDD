5f3186a2ffd3576ccf215dccc0497c68b2a3f00e
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-06-29 11:06:29 +0300

    Modify test and function

--- solver.rb
+++ solver.rb
@@ -1,7 +1,9 @@
 class Solver
-    def fizz_buzz(n)
-        return 'FizzBuzz' if ( n % 3 == 0 && n % 5 == 0)
-        return 'Fizz' if ( n % 3).zero?
-        return 'Buzz' if ( n % 5).zero?
+    def fizz_buzz(number)
+        return 'FizzBuzz' if ( number % 3 == 0 && number % 5 == 0)
+        return 'Fizz' if ( number % 3).zero?
+        return 'Buzz' if ( number % 5).zero?
+
+        number
     end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -15,5 +15,9 @@ describe Solver do
         it 'return "Fizz" on numbers divisible by 3' do
             expect(solver.fizz_buzz(15)).to eq('FizzBuzz')
         end
+
+        it 'return number on numbers not divisible by 3 or 5' do
+            expect(solver.fizz_buzz(11)).to eq(11)
+        end
     end
 end
