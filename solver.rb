9cb0a7629afd0427420c11dc67c9fe0ec82993b2
Abbas Sarwar <abbassarwar100@gmail.com>
2023-06-29 19:39:03 +0500

    Linter errors fixed

--- solver.rb
+++ solver.rb
@@ -1,25 +1,30 @@
+# frozen_string_literal: true
+
+# Solver is a class that provides various solving functionalities.
+# It includes methods for calculating factorials, playing the FizzBuzz game,
+# and reversing strings
 class Solver
-    def factorial(n)
-        if(n < 0 )
-            raise ArgumentError, "Negav not allow"
-        elsif(n == 0)
-            return 1
-        else
-            results = 1
-            (1..n).each {|a| results *= a }
-            return results
-        end
+  def factorial(num)
+    if num.negative?
+      raise ArgumentError, 'Negative not allow'
+    elsif num.zero?
+      1
+    else
+      results = 1
+      (1..num).each { |a| results *= a }
+      results
     end
+  end
 
-    def fizz_buzz(number)
-        return 'FizzBuzz' if ( number % 3 == 0 && number % 5 == 0)
-        return 'Fizz' if ( number % 3).zero?
-        return 'Buzz' if ( number % 5).zero?
+  def fizz_buzz(number)
+    return 'FizzBuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'Fizz' if (number % 3).zero?
+    return 'Buzz' if (number % 5).zero?
 
-        number
-    end
+    number
+  end
 
-    def reverse (word) 
-        word.split('').reverse.join('')
-    end
+  def reverse(word)
+    word.split('').reverse.join('')
+  end
 end
