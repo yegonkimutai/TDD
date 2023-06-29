eff0f7ae8dae4285cc740334efcafebbcff06bd7
Abbas Sarwar <abbassarwar100@gmail.com>
2023-06-29 19:15:28 +0500

    Factorial TDD

--- solver.rb
+++ solver.rb
@@ -1,4 +1,16 @@
 class Solver
+    def factorial(n)
+        if(n < 0 )
+            raise ArgumentError, "Negav not allow"
+        elsif(n == 0)
+            return 1
+        else
+            results = 1
+            (1..n).each {|a| results *= a }
+            return results
+        end
+    end
+
     def fizz_buzz(number)
         return 'FizzBuzz' if ( number % 3 == 0 && number % 5 == 0)
         return 'Fizz' if ( number % 3).zero?
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,6 +3,23 @@ require_relative '../solver'
 describe Solver do
     let(:solver) { Solver.new }
 
+    describe 'Factorial' do
+        it 'returns 1 for factorial of 0' do
+            expect(solver.factorial(0)).to eq(1)
+          end
+      
+          it 'returns the correct factorial for positive integers' do
+            expect(solver.factorial(5)).to eq(120)
+            expect(solver.factorial(6)).to eq(720)
+            expect(solver.factorial(10)).to eq(3628800)
+          end
+      
+          it 'raises an exception for negative integers' do
+            expect { solver.factorial(-1) }.to raise_error(ArgumentError)
+            expect { solver.factorial(-5) }.to raise_error(ArgumentError)
+          end
+        end
+
     describe 'FizzBuzz challenge' do
         it 'return "Fizz" on numbers divisible by 3' do
             expect(solver.fizz_buzz(9)).to eq('Fizz')
