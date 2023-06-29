9622f0a6ea83fc8c44206a9ff40e5ae2a9ad63b8
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-06-29 11:02:26 +0300

    Add FizzBuzz method

--- solver.rb
+++ solver.rb
@@ -1,2 +1,7 @@
 class Solver
+    def fizz_buzz(n)
+        return 'FizzBuzz' if ( n % 3 == 0 && n % 5 == 0)
+        return 'Fizz' if ( n % 3).zero?
+        return 'Buzz' if ( n % 5).zero?
+    end
 end
